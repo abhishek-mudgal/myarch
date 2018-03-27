12096a90c33e2e24b372a561260de33bb754115c
abhishek <abhishek.mudgal.59@gmail.com>
2018-03-27 14:01:27 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,9 +1,6 @@
 echo "OPTIMUS" > /etc/hostname
 echo "en_IN UTF-8" >> /etc/locale.gen
 
-cd /home/abhishek/
-rm -rf .android .bash_history .bash_logout .bash_profile .bashrc .cache .config .designer .dmrc .esd_auth .Genymobile .gitconfig .bash_profile .gtkrc-2.0 .icons .imwheelrc .local .mozilla .nv .pki .repoconfig .repo_.gitconfig.json .stremio-server .ssh .themes .vim .viminfo .Xauthority .xsession-errors .xsession-errors.old
-cd -
 
 locale-gen
 echo LANG=en_IN.UTF-8 > /etc/locale.conf
@@ -16,10 +13,6 @@ echo "" >> /etc/pacman.conf
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 
-echo "" >> /etc/pacman.conf
-echo "[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
 
 
 pacman -Syyu
@@ -29,7 +22,7 @@ useradd -mg users -G wheel,storage,power -s /bin/bash abhishek
 passwd abhishek
 chage -d 0 abhishek
 
-pacman -S sudo curl ppp netctl dialog wpa_supplicant networkmanager grub efibootmgr dosfstools os-prober vim nano curl wget vlc pulseaudio cheese pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack htop gparted mtools yaourt xorg-server xorg-xinit gdm gnome gnome-extra bluez bluez-utils network-manager-applet firefox-developer-edition bumblebee mesa xf86-video-intel nvidia lib32-nvidia-utils bbswitch nvidia-utils nvidia-settings cuda
+pacman -S sudo curl ppp netctl dialog wpa_supplicant networkmanager grub efibootmgr dosfstools os-prober vim nano curl wget vlc pulseaudio cheese pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack htop gparted mtools xorg-server xorg-xinit lightdm gdm deepin deepin-extra bluez bluez-utils firefox-developer-edition bumblebee mesa xf86-video-intel nvidia lib32-nvidia-utils bbswitch nvidia-utils nvidia-settings cuda
 mkdir /boot/EFI
 mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
@@ -38,9 +31,14 @@ grub-mkconfig -o /boot/grub/grub.cfg
 gpasswd -a abhishek bumblebee
 gpasswd -a root bumblebee
 
-systemctl enable gdm
+systemctl enable lightdm
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable bumblebeed.service
+
+echo "" >> /etc/pacman.conf
+echo "[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
 
 visudo
