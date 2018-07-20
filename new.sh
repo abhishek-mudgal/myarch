ad794ca7b326bcbbe74ed86a836cb345c7c56245
abhishek <abhishek.mudgal.59@gmail.com>
2018-07-20 21:18:33 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -3,18 +3,17 @@
 
 
 wget https://raw.githubusercontent.com/abhishek9650/myarch/master/afterinstall.sh
-wget https://raw.githubusercontent.com/abhishek9650/myarch/master/mirrorlist
-cp mirrorlist /etc/pacman.d/mirrorlist
 
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sda4
+mkfs.ext4 /dev/sda5
 
 mkswap /dev/sda2
 
 mount /dev/sda3 /mnt
 mkdir /mnt/home
-mount /dev/sda5 /mnt/home
-rm -rf /mnt/home/abhishek/.*
+mount /dev/sda4 /mnt/home
 swapon /dev/sda2
 
 echo "" >> /etc/pacman.conf
@@ -25,8 +24,6 @@ echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacstrap /mnt base base-devel
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
-
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
 cp afterinstall.sh /mnt/
 arch-chroot /mnt
