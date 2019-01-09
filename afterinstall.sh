0c49df0ba7756a0ccfa8949febeb1c2abb4982d4
abhishek <abhishek.mudgal.59@gmail.com>
2019-01-09 14:38:03 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -23,12 +23,16 @@ passwd abhishek
 
 pacman -S sudo curl ppp netctl dialog wpa_supplicant networkmanager  \
           grub efibootmgr dosfstools os-prober    \
-          vim nano curl wget vlc pulseaudio cheese hexchat telegram-desktop   \
+          vim nano curl wget vlc pulseaudio cheese hexchat telegram-desktop flashplugin pepper-flash   \
+          virtualbox  virtualbox-guest-iso virtualbox-host-modules-arch   \
+          gvfs gvfs-mtp gvfs-google gvfs-goa gvfs-afc gvfs-smb gvfs-gphoto2   \
           pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack    \
           htop gparted mtools xorg lightdm gedit bluez bluez-utils firefox   \
+          intel-gmmlib intel-media-driver intel-tbb lib32-libva-intel-driver lib32-libva1-intel-driver lib32-vulkan-intel libva-intel-driver    \
+          libva1-intel-driver vulkan-intel acpica beignet    \
           bumblebee mesa xf86-video-intel nvidia lib32-nvidia-utils bbswitch nvidia-utils nvidia-settings cuda   \
           ttf-ubuntu-font-family ubuntu-keyring    \
-          deepin deepin-extra gtk-engine-murrine gtk-engines qtcreator
+          gnome gtk-engine-murrine gtk-engines qtcreator
 mkdir /boot/EFI
 mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
@@ -37,7 +41,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 gpasswd -a abhishek bumblebee
 gpasswd -a root bumblebee
 
-systemctl enable lightdm
+systemctl enable gdm
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable bumblebeed.service
